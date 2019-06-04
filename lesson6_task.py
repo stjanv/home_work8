429de9484411dcc51d387cb814a14e6e80d903cb
stjanv <ruslanstojnov@gmail.com>
2019-06-04 19:31:45 +0300

    update

--- lesson6_task.py
+++ lesson6_task.py
@@ -1,29 +1,36 @@
 from zipfile import ZipFile
 from os import mkdir
+import string
+import itertools
 
 line = '-----------------------------'
 
-
-def generator(string):
-    for word in string:
-        password = word.replace('\n','')
-        archive.setpassword(password.encode())
-        try:
-            archive.extractall(directory)
-        except:
-            yield "[False]:" + password
-        else:
-            yield line + "\n[True]: " + password; return
+def generator(chars):
+    #attempts = 0
+    for password_length in range(1, 9):
+        for password in itertools.product(chars, repeat=password_length):
+            #attempts += 1
+            password = ''.join(password)
+            archive.setpassword(password.encode())
+            try:
+                archive.extractall(directory)
+            except:
+                yield "[False]:" + password
+            else:
+                yield line + "\n[True]: " + password
+                return
 
 
 directory = "ExtractArchive"
-try:mkdir(directory)
-except FileExistsError: pass
+try:
+    mkdir(directory)
+except FileExistsError:
+    pass
 
 print(line)
-with open(input("Dictionary: "), errors='ignore') as dictionary:
-    with ZipFile(input("Archive: ")) as archive:
-        print(line)
-        for password in generator(dictionary):
-            print(password)
+chars = string.ascii_lowercase + string.digits
+with ZipFile(input("Archive: ")) as archive:
+    print(line)
+    for password in generator(chars):
+        print(password)
 print(line)
